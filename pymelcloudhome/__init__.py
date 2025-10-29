b1bbf3806f94ddea402d5559676d6259fc299218
Martin Hultman <maartin.hultman@gmail.com>
2025-10-29 10:48:18 +0100

    chore: Bump version to 0.3.1 in __init__.py and pyproject.toml

--- pymelcloudhome/__init__.py
+++ pymelcloudhome/__init__.py
@@ -3,5 +3,5 @@
 from . import errors, models, services
 from .client import MelCloudHomeClient
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __all__ = ["MelCloudHomeClient", "models", "errors", "services", "__version__"]
