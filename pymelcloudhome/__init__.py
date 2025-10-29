fe708a355a74e3f66f8fb273b70004dffc469002
Martin Hultman <maartin.hultman@gmail.com>
2025-10-29 10:45:28 +0100

    chore: Update version to 0.3.0 in __init__.py

--- pymelcloudhome/__init__.py
+++ pymelcloudhome/__init__.py
@@ -3,5 +3,5 @@
 from . import errors, models, services
 from .client import MelCloudHomeClient
 
-__version__ = "0.1.1"
+__version__ = "0.3.0"
 __all__ = ["MelCloudHomeClient", "models", "errors", "services", "__version__"]
